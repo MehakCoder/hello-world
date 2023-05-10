88eadef17eb8ed9d886ba466a68cdf86f36c38b8
MehakCoder <100033814+MehakCoder@users.noreply.github.com>
2023-05-10 22:52:12 +0530

    add new code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Profile from './components/Profile';
+import Message from './components/Message';
+import Counter from './components/Counter';
+import Resume from './components/Resume';
+import FunctionEvent from './components/FunctionEvent';
+import ClassEvent from './components/ClassEvent';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Counter></Counter>
     </div>
   );
 }
